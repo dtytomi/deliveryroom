d8a7334f26c31112acefa20fb93a612e4c9aceab
dtytomi <dtytomi@gmail.com>
2017-01-02 16:39:09 +0100

    watch and pray

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -1,30 +1,28 @@
 'use strict';
 
 var passport = require('passport'),
-  authentication = require('../controllers/authentication.js');
+  express = require('express'),
+  authentication = require('../controllers/authentication.js'),
+  router = express.Router();
+
+router.get('/signout', authentication.signout);
+
+router.get('/instagram', passport.authenticate('instagram'));
+router.get('/instagram/callback', authentication.oauthCallback('instagram'));
+router.get('/twitter', passport.authenticate('twitter'));
+router.get('/twitter/callback', authentication.oauthCallback('twitter'));
+router.get('/google', passport.authenticate('google', {
+  scope: [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ]
+}));
+router.get('/google/callback', authentication.oauthCallback('google'));
+router.get('/facebook', passport.authenticate('facebook', {
+  scope: ['email']
+}));
+router.get('/facebook/callback', authentication.oauthCallback('facebook'));
 
 
-module.exports = function(app) {
-
-  app.route('/auth/signout').get(authentication.signout);
-
-  app.route('/auth/twitter').get(passport.authenticate('twitter'));
-  app.route('/auth/twitter/callback').get(authentication.oauthCallback('twitter'));
-
-  app.route('/auth/google').get(passport.authenticate('google', {
-    scope: [
-      'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
-  }));
-  app.route('/auth/google/callback').get(authentication.oauthCallback('google'));
-
-  app.route('/auth/facebook').get(passport.authenticate('facebook', {
-    scope: ['email']
-  }));
-  app.route('/auth/facebook/callback').get(authentication.oauthCallback('facebook'));
-
-  app.route('/auth/instagram').get(passport.authenticate('instagram'));
-  app.route('/auth/instagram/callback').get(authentication.oauthCallback('instagram'));
-};
+module.exports = router;
