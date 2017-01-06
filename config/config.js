c6649f04e01947ad5164e82aed0fac0198ef87f0
dtytomi <dtytomi@gmail.com>
2017-01-06 08:22:52 +0100

    config.js refactored to reflect mongo url online by updating production

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'server'
     },
     port: process.env.PORT || 3000,
-    db:  process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/server-development',
+    db: 'mongodb://127.0.0.1:27017/server-development',
     facebook: {
       clientID: process.env.FACEBOOK_ID || 'FACEBOOK_ID',
       clientSecret: process.env.FACEBOOK_SECRET || 'FACEBOOK_SECRET',
@@ -50,7 +50,30 @@ var config = {
       name: 'server'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://127.0.0.1:27017/server-production'
+    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI ||'mongodb://127.0.0.1:27017/server-production',
+    facebook: {
+      clientID: process.env.FACEBOOK_ID || 'FACEBOOK_ID',
+      clientSecret: process.env.FACEBOOK_SECRET || 'FACEBOOK_SECRET',
+      callbackURL: 'https://watchandpray.herokuapp.com/auth/facebook/callback' ||'http://localhost:3000/auth/facebook/callback'
+    },
+
+    twitter: {
+      clientID: process.env.TWITTER_CONSUMER_KEY || 'TWITTER_CONSUMER_KEY',
+      clientSecret: process.env.TWITTER_CONSUMER_SECRET || 'TWITTER_CONSUMER_SECRET',
+      callbackURL:  'https://watchandpray.herokuapp.com/auth/twitter/callback'|| 'http://localhost:3000/auth/twitter/callback'
+    },
+
+    google: {
+      consumerKey: process.env.GOOGLE_CONSUMER_KEY || 'GOOGLE_CONSUMER_KEY',
+      consumerSecret: process.env.GOOGLE_CONSUMER_SECRET || 'GOOGLE_CONSUMER_SECRET',
+      callbackURL: 'https://watchandpray.herokuapp.com/auth/google/callback' ||'http://localhost:3000/auth/google/callback'
+    },
+
+    instagram: {
+      clientID: process.env.ClientID || 'INSTAGRAM_ID',
+      clientSecret: process.env.ClientSecret || 'INSTAGRAM_SECRET',
+      callbackURL: 'https://watchandpray.herokuapp.com/auth/instagram/callback' ||'http://localhost:3000/auth/instagram/callback'
+    }
   }
 };
 
