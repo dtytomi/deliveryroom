30c0ea5a3b475ccb41d5ea87a4790322629ea437
dtytomi <dtytomi@gmail.com>
2017-01-22 04:21:47 +0100

    getOauthToken disabled

--- app/routes/user.js
+++ app/routes/user.js
@@ -19,8 +19,7 @@ router.get('/auth/facebook', passport.authenticate('facebook', {
   scope: ['email']
 }));
 router.get('/auth/facebook/callback', passport.authenticate('facebook'));
-router.get('/auth/facebook/callback', authentication.getOauthToken);
-router.get('/auth/instagram', passport.authenticate('instagram'));
+// router.get('/auth/instagram', passport.authenticate('instagram'));
 router.get('/auth/instagram/callback',  passport.authenticate('instagram'));
 router.get('/auth/instagram/callback', authentication.getOauthToken);
 router.get('/auth/google', passport.authenticate('google', {
@@ -34,7 +33,7 @@ router.get('/auth/google/callback', passport.authenticate('google'));
 router.get('/auth/google/callback', authentication.getOauthToken);
 router.get('/auth/twitter', passport.authenticate('twitter'));
 router.get('/auth/twitter/callback', passport.authenticate('twitter'));
-router.get('/auth/twitter/callback', authentication.getOauthToken);
+// router.get('/auth/twitter/callback', authentication.getOauthToken);
 
 router.get('/api/user', account.user );
 router.get('/api/users/me', account.me );
