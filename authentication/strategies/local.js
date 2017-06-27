e21af6014658285a0c1a15f5f8598712728d7ca1
dtytomi <dtytomi@gmail.com>
2017-06-28 00:31:25 +0100

    made a minor update I changed 'Bearer' int isAuthenticated to

---
+++ authentication/strategies/local.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+
+function localStrategy (User, config) {
+  // body...
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+    function (email, password, callback) {
+      // body...
+      User.findOne({
+        email: email.toLowerCase()
+      }, function (err, user) {
+        // body...
+        if (err) return callback(err);
+
+        // no user found with that email
+        if (!user) {
+          return callback(null, false, { message: 'The email is not registered.'});
+        }
+
+        // make sure the password is correct 
+        user.comparePassword(password, function (err, isMatch) {
+          // body...
+          if (err) { return callback(err); }
+
+          // password did not match
+          if (!isMatch) {
+              return callback(null, false, {message: 'The password is not correct.'});
+          }
+
+          // success
+          return callback(null, user)
+        });
+
+      });
+
+    })
+
+  );
+}
+
+module.exports = localStrategy;
